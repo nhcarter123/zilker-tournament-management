e806fbf30f4e07f10c29b7ff010dc3f251a3231a
Nathaniel Carter <nhcarer@wpi.edu>
2022-08-17 09:59:07 -0500

    add rate limiting error message

--- src/components/pages/LoginPage/index.tsx
+++ src/components/pages/LoginPage/index.tsx
@@ -6,7 +6,7 @@ import { SEND_VERIFICATION_CODE } from 'graphql/definitions/mutations';
 import SendCodeForm from 'components/forms/SendCodeForm';
 import CodeInput from 'components/CodeInput';
 
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 
 interface LoginPageProps {
   verifyCode: Function;
@@ -14,30 +14,47 @@ interface LoginPageProps {
 
 const LoginPage = ({ verifyCode }: LoginPageProps): JSX.Element => {
   const [hasSentCode, setHasSentCode] = useState(false);
+  const [tooManyCodesError, setTooManyCodesError] = useState(false);
 
   const [sendVerificationCode, { loading: verificationCodeLoading }] =
-    useMutation(SEND_VERIFICATION_CODE, { onError });
+    useMutation(SEND_VERIFICATION_CODE, {
+      onError: (error) => {
+        setTooManyCodesError(error.message.includes('Rate limit exceeded'));
+        onError(error);
+      }
+    });
 
   return (
     <div>
-      <Typography variant={'h4'} align={'center'}>
-        Welcome 👋
-      </Typography>
-      <Typography
-        variant={'body2'}
-        align={'center'}
-        sx={{ marginBottom: '-12px', color: 'gray' }}
-      >
-        new or existing user sign in
-      </Typography>
-      <SendCodeForm
-        hasSentCode={hasSentCode}
-        setHasSentCode={setHasSentCode}
-        sendVerificationCode={sendVerificationCode}
-        loading={verificationCodeLoading}
-      />
-      {hasSentCode && !verificationCodeLoading && (
-        <CodeInput verifyCode={verifyCode} />
+      {tooManyCodesError ? (
+        <>
+          <Typography variant={'h5'}>⚠️ Rate limit exceeded ⚠️</Typography>
+          <Box display={'flex'} justifyContent={'center'}>
+            <Typography variant={'body2'}>Come back later️</Typography>
+          </Box>
+        </>
+      ) : (
+        <div>
+          <Typography variant={'h4'} align={'center'}>
+            Welcome 👋
+          </Typography>
+          <Typography
+            variant={'body2'}
+            align={'center'}
+            sx={{ marginBottom: '-12px', color: 'gray' }}
+          >
+            New or existing user sign in
+          </Typography>
+          <SendCodeForm
+            hasSentCode={hasSentCode}
+            setHasSentCode={setHasSentCode}
+            sendVerificationCode={sendVerificationCode}
+            loading={verificationCodeLoading}
+          />
+          {hasSentCode && !verificationCodeLoading && (
+            <CodeInput verifyCode={verifyCode} />
+          )}
+        </div>
       )}
     </div>
   );
